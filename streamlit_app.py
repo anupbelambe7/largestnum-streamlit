a6b92816978f51ee343519f4a516eef80fc898d9
Anup Avinashrao Belambe <140591249+anupbelambe7@users.noreply.github.com>
2023-08-28 16:43:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ This application finds the largest among the 3 given numbers
 num1 = st.number_input("First Number:",step=1)
 num2 = st.number_input("Second Number:",step=1)
 num3 = st.number_input("Third Number:",step=1)
-if num1 > num2 and num1 > num3:
+if num1 >= num2 and num1 >= num3:
     largest = num1
-elif num2 > num1 and num2 > num3:
+elif num2 >= num1 and num2 >= num3:
     largest = num2
 else:
     largest = num3
