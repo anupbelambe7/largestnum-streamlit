2adfecca9521f9acc3c4c6eac1ac5545da59f6e2
Anup Avinashrao Belambe <140591249+anupbelambe7@users.noreply.github.com>
2023-08-28 16:45:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.header("Find Largest Number App")
 """
 This application finds the largest among the 3 given numbers
+---
 """
 num1 = st.number_input("First Number:",step=1)
 num2 = st.number_input("Second Number:",step=1)
@@ -13,7 +14,7 @@ elif num2 >= num1 and num2 >= num3:
     largest = num2
 else:
     largest = num3
-st.divider() 
+"---"
 st.metric(label="**Largest Number**",value=largest)
 ans = f"""
 The largest among the numbers {num1},{num2},{num3} is **:blue[{largest}]**"""
