8665c6c86c0a286d904c95746a8ff9f99ca65d51
Anup Avinashrao Belambe <140591249+anupbelambe7@users.noreply.github.com>
2023-08-28 16:09:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ elif num2 > num1 and num2 > num3:
 else:
     largest = num3
 st.divider()    
-st.subheader("The largest among the 3 given numbers is")    
+st.subheader("The largest among the 3 given numbers is", largest)    
 st.metric(label="",value=largest)
